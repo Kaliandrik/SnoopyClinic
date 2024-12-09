28d9a7b8d6d173d6a4976c4b6c1337fb2dc35d86
Kaliandrik <contatokaliandrik@gmail.com>
2024-12-08 23:12:50 -0300

    Site atualizado

--- pages/1_Suas informações.py
+++ pages/1_Suas informações.py
@@ -28,45 +28,47 @@ def create_shaking_effect(image_path, output_path, shakes=10, intensity=5):
 
 
 create_shaking_effect("img/natalsidebar.jpg", "img/natalsidebar_tremendo.gif")
-col1, col2, col3 = st.columns([1, 2, 1])
+if "nome" in st.session_state and "clinica" in st.session_state and "email" in st.session_state and "pet" in st.session_state:
 
-with col2:
-    # Adicionar uploader de imagem
-    st.subheader("Foto de Perfil")
-    uploaded_file = st.file_uploader("Clique para escolher uma imagem", type=["png", "jpg", "jpeg"])
+    col1, col2, col3 = st.columns([1, 2, 1])
 
-    # Se uma imagem for carregada, salvá-la no session_state
-    if uploaded_file is not None:
-        st.session_state["uploaded_image"] = uploaded_file
+    with col2:
+        # Adicionar uploader de imagem
+        st.subheader("Foto de Perfil")
+        uploaded_file = st.file_uploader("Clique para escolher uma imagem", type=["png", "jpg", "jpeg"])
 
-    # Preparar a imagem a ser exibida (carregada ou padrão)
-    if st.session_state["uploaded_image"]:
-        # Converter o arquivo carregado pelo usuário para base64
-        uploaded_file_content = st.session_state["uploaded_image"].getvalue()
-        encoded_image = base64.b64encode(uploaded_file_content).decode()
-    else:
-        # Converter a imagem padrão para base64
-        with open("img/usuario3.png", "rb") as f:
-            encoded_image = base64.b64encode(f.read()).decode()
+        # Se uma imagem for carregada, salvá-la no session_state
+        if uploaded_file is not None:
+            st.session_state["uploaded_image"] = uploaded_file
 
-    # Exibir a imagem com estilo circular
-    st.markdown(
-        f"""
-        <style>
-        .profile-pic {{
-            display: block;
-            margin: 0 auto;
-            border-radius: 50%;
-            width: 200px;
-            height: 200px;
-            object-fit: cover;
-            border: 5px solid #888cba;
-        }}
-        </style>
-        <img class="profile-pic" src="data:image/jpeg;base64,{encoded_image}">
-        """,
-        unsafe_allow_html=True,
-    )
+        # Preparar a imagem a ser exibida (carregada ou padrão)
+        if st.session_state["uploaded_image"]:
+            # Converter o arquivo carregado pelo usuário para base64
+            uploaded_file_content = st.session_state["uploaded_image"].getvalue()
+            encoded_image = base64.b64encode(uploaded_file_content).decode()
+        else:
+            # Converter a imagem padrão para base64
+            with open("img/usuario3.png", "rb") as f:
+                encoded_image = base64.b64encode(f.read()).decode()
+
+        # Exibir a imagem com estilo circular
+        st.markdown(
+            f"""
+            <style>
+            .profile-pic {{
+                display: block;
+                margin: 0 auto;
+                border-radius: 50%;
+                width: 300px;
+                height: 300px;
+                object-fit: cover;
+                border: 5px solid #888cba;
+            }}
+            </style>
+            <img class="profile-pic" src="data:image/jpeg;base64,{encoded_image}">
+            """,
+            unsafe_allow_html=True,
+        )
 
 st.title("Suas Informações:")
 st.sidebar.image("img/natalsidebar_tremendo.gif")
